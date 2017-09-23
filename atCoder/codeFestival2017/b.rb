30b1e2aaf84e31da582d94938aeae8507c3474ae
ueyuki <s14t212@stmail.eng.kagawa-u.ac.jp>
2017-09-24 02:27:31 +0900

    AC problem B of codeFestival2017 qual A in atcoder

--- atCoder/codeFestival2017/b.rb
+++ atCoder/codeFestival2017/b.rb
@@ -1,3 +1,12 @@
 n, m, k = gets.chomp.split.map(&:to_i)
 
-if 
+n.times do |a|
+  m.times do |b|
+    if (a * (m - b) + b * (n - a)) == k
+      puts 'Yes'
+      exit
+    end
+  end
+end
+
+puts 'No'
