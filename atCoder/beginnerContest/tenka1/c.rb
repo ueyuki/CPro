467d24c3005ab45fd7d195984948b48b673dbda9
ueyuki <s14t212@stmail.eng.kagawa-u.ac.jp>
2017-10-04 14:36:53 +0900

    TLE problem c of tenka1BeginnerContest

--- atCoder/beginnerContest/tenka1/c.rb
+++ atCoder/beginnerContest/tenka1/c.rb
@@ -2,8 +2,17 @@ N = gets.chomp.to_i
 ans = []
 if N % 2 == 0
   ans = [N, N, N/2]
-  puts ans.join(' ')
 else
-  ans = [1, 1, 1]
-  puts ans.join(' ')
+  (1..3500).each do |h|
+    (1..3500).each do |w|
+      x = N * h * w
+      y = 4 * h * w - N * h - w * N
+      if y > 0 && x % y == 0
+        ans = [h, w, x / y]
+        break
+      end
+    end
+  end
 end
+
+puts ans.join(' ')
