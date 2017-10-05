b4bbaef68eced4cfc0594f8471a0f7c0e08e4b66
ueyuki <s14t212@stmail.eng.kagawa-u.ac.jp>
2017-10-05 14:11:06 +0900

    AC problem c of tenka1BeginnerContst

--- atCoder/beginnerContest/tenka1/c.rb
+++ atCoder/beginnerContest/tenka1/c.rb
@@ -2,6 +2,7 @@ N = gets.chomp.to_i
 ans = []
 if N % 2 == 0
   ans = [N, N, N/2]
+  puts ans.join(' ')
 else
   (1..3500).each do |h|
     (1..3500).each do |w|
@@ -9,10 +10,10 @@ else
       y = 4 * h * w - N * h - w * N
       if y > 0 && x % y == 0
         ans = [h, w, x / y]
-        break
+        puts ans.join(' ')
+        exit
       end
     end
   end
 end
 
-puts ans.join(' ')
