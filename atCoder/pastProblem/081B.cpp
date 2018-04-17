26fc4e8c1c170c6ee1506fcd635c361c477427b3
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-04-17 10:30:11 +0900

    AC AtCoder PastProblem 081 B

---
+++ atCoder/pastProblem/081B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  vector<int> A;
+  int N, count = 0;
+
+  scanf("%d", &N);
+  for (int k = 0; k < N; k++)
+  {
+    int num;
+    
+    scanf("%d", &num);
+    A.push_back(num);
+  }
+
+  while (1)
+  {
+    for (int k = 0; k < N; k++)
+    {
+      if (A[k] % 2 == 0) 
+      {
+        A[k] = A[k] / 2;
+      }
+      else
+      {
+        printf("%d\n", count);
+        return 0;
+      }
+    }
+    count++;
+  }
+
+}
