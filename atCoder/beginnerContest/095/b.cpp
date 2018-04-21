e7ed5fc2330b374adf9e4a860949ceba45312677
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-04-21 22:56:55 +0900

    AC AtCoder begginerContest 095 problem B

--- atCoder/beginnerContest/095/b.cpp
+++ atCoder/beginnerContest/095/b.cpp
@@ -4,29 +4,18 @@ using namespace std;
 int main() {
   int N, X;
   scanf("%d %d", &N, &X);
-  vector<int> m;
-  int ans = N, min, usePow = 0;
+  int ans = N, min = 1001, usePow = 0;
 
   for (int k = 0; k < N; k++) {
     int t;
     scanf("%d", &t);
-    m.push_back(t);
+    if (min > t) min = t;
     usePow += t;
   }
 
-  min = m[0];
+  ans += (X - usePow) / min;
 
-  for (int k = 1; k < m.size(); k++) {
-    if (min > m[k]) min = m[k];
-  }
-
-  while (1) {
-    usePow += min;
-    if (usePow < X) ans++;
-    else break;
-  }
-
-  printf("%d %d\n", ans, usePow);
+  printf("%d\n", ans);
 
   return 0;
 }
