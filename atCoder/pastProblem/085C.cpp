a15466de612aa7d4ac9b0ee51b718867aeb48b30
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-04-27 01:46:42 +0900

    TLE pastProblem ABC 085 C

--- atCoder/pastProblem/085C.cpp
+++ atCoder/pastProblem/085C.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main() {
-  int n, y;
+  int n, y, sum;
+  int l = 0, m = 0, o = 0;
 
-  cin >> n >> y;
+  scanf("%d %d", &n, &y);
+
+  for (l = 0; l <= y / 10000; l++) {
+    for (m = 0; m <= (y - (10000 * l)) / 5000; m++) {
+      for (o = 0; o <= (y - (10000 * l + 5000 * m)) / 1000; o++) {
+        sum = (10000 * l) + (5000 * m) + (1000 * o);
+        if (sum == y && (l + m + o) == n) {
+          printf("%d %d %d\n", l, m, o);
+          return 0;
+        }       
+      }
+    }
+  }
+
+  printf("-1 -1 -1\n");
+  return 0;
 
 }
