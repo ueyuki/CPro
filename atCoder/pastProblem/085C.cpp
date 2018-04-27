8155714e349d1f1df6715d1c676d67be55bd8ba9
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-04-27 20:51:04 +0900

    AC pastProblem ABC 085 C

--- atCoder/pastProblem/085C.cpp
+++ atCoder/pastProblem/085C.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main() {
-  int n, y, sum;
+  int n, y;
   int l = 0, m = 0, o = 0;
 
   scanf("%d %d", &n, &y);
 
   for (l = 0; l <= y / 10000; l++) {
     for (m = 0; m <= (y - (10000 * l)) / 5000; m++) {
-      for (o = 0; o <= (y - (10000 * l + 5000 * m)) / 1000; o++) {
-        sum = (10000 * l) + (5000 * m) + (1000 * o);
-        if (sum == y && (l + m + o) == n) {
-          printf("%d %d %d\n", l, m, o);
-          return 0;
-        }       
-      }
+      o = (y - (10000 * l + 5000 * m)) / 1000;
+      if ((m + l + o) == n) {
+        printf("%d %d %d\n", l, m, o);
+        return 0;
+      } 
     }
   }
 
