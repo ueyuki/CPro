486cc1693c42bddde888986a5e66b339179e0791
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-04-29 02:47:48 +0900

    WA ABC pastProblem 049 C

--- atCoder/pastProblem/049C.cpp
+++ atCoder/pastProblem/049C.cpp
@@ -4,4 +4,29 @@ using namespace std;
 int main() {
   string s;
   cin >> s;
+  int pos = 0;
+
+  while (1) {
+    if (pos >= (int)s.length()) break;
+    printf("%d", pos);
+    if ((int)s.find("dreamer", pos) == pos) {
+      pos += 7; 
+      continue;
+    }
+    if ((int)s.find("dream", pos) == pos) {
+      pos += 5; continue;
+    }
+    if ((int)s.find("eraser", pos) == pos) { 
+      pos += 6; continue;
+    }
+    if ((int)s.find("erase", pos) == pos) {
+      pos += 5; continue;
+    }
+    break;
+  }
+
+  if (pos == (int)s.length()) cout << "YES" << endl;
+  else cout << "NO" << endl;
+
+  return 0;
 }
