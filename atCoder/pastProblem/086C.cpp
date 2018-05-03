52a3c2e9e2eb636d41c8773fda69d67790172d02
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-05-03 18:44:51 +0900

    AC pastProblem ABC 086 C

--- atCoder/pastProblem/086C.cpp
+++ atCoder/pastProblem/086C.cpp
@@ -3,4 +3,27 @@ using namespace std;
 
 int main() {
   int n;
+  scanf("%d", &n);
+  vector<int> tt, xx, yy;
+  tt.push_back(0);
+  xx.push_back(0);
+  yy.push_back(0);
+
+  for (int k = 0; k < n; k++) {
+    int t, x, y;
+    scanf("%d %d %d", &t, &x, &y);
+    tt.push_back(t);
+    xx.push_back(x);
+    yy.push_back(y);
+    t = tt[k+1] - tt[k];
+    x = xx[k+1] - xx[k];
+    y = yy[k+1] - yy[k];
+    if (t < x + y || (t - (x + y)) % 2 == 1) {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+
+  cout << "Yes" << endl;
+  return 0;
 }
