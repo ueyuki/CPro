2311628c1178d14a3213d55ea9efca106075a4e9
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-05-04 23:51:47 +0900

    CE pastProblem ABC 075 B

---
+++ atCoder/pastProblem/075B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  string s[h][w];
+
+  for (int k = 0; k < h; k++) {
+    for (int l = 0; l < w; l++) {
+      cin >> s[k][l];
+    }
+  }
+
+  for (int k = 0; k < h; k++) {
+    for (int l = 0; l < w; l++) {
+      if (s[k][l] == '.') {
+        int count = 0;
+        for (int m = -1; m < 2; m++) {
+          for (int n = -1; n < 2; n++) {
+            if (k + m >= 0 && k + m <= h &&
+                l + n >= 0 && l + n <= w &&
+                s[k + m][l + n] == '#') count++;
+          }
+        }
+      }
+      cout << s[k][l] << endl;
+    }
+  }
+
+
+  return 0;
+}
