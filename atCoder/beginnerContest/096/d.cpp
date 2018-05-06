4ab3e1aae70981179a8938c0c6eb9720fc327772
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-05-06 10:22:22 +0900

    AC ABC 096 D

--- atCoder/beginnerContest/096/d.cpp
+++ atCoder/beginnerContest/096/d.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPrime(int num)
-{
+bool isPrime(int num) {
     if (num < 2) return false;
     else if (num == 2) return true;
-    else if (num % 2 == 0) return false; // 偶数はあらかじめ除く
+    else if (num % 2 == 0) return false;
 
     double sqrtNum = sqrt(num);
-    for (int i = 3; i <= sqrtNum; i += 2)
-    {
-        if (num % i == 0)
-        {
-            // 素数ではない
+    for (int i = 3; i <= sqrtNum; i += 2) {
+        if (num % i == 0) {
             return false;
         }
     }
-
-    // 素数である
     return true;
 }
 
 int main() {
   int n;
   cin >> n;
-  int ans[n];
 
+  for (int k = 11; k <= 55555; k += 10) {
+    if (isPrime(k)) {
+      cout << k;
+      n--;
+      if (n == 0) break;
+      cout << ' ';
+    }
+  }
+  cout << endl;
+  return 0;
 }
