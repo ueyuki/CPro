535b7146b475771672d6423b6845ff174fe9076b
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-05-08 23:21:05 +0900

    Refactor ABC 070 B

--- atCoder/pastProblem/070B.cpp
+++ atCoder/pastProblem/070B.cpp
@@ -5,12 +5,10 @@ int main() {
   int a, b, c, d;
   cin >> a >> b >> c >> d;
 
-  if (a > d) cout << '0' << endl;
-  else if (b < c) cout << '0' << endl;
-  else if (a >= c && a <= d && b >= d) cout << d - a << endl;
-  else if (b >= c && b <= d && a <= c) cout << b - c << endl;
-  else if (a < c && b > d) cout << d - c << endl;
-  else if (a > c && b < d) cout << b - a << endl;
+  int lower = max(a, c);
+  int upper = min(b, d);
+
+  cout << max(0, upper - lower) << endl; 
 
   return 0;
 }
