4667b009724b20258ed4ffdc682ad8e2235b39ca
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-05-11 21:47:59 +0900

    AC ABC 087 C

--- atCoder/pastProblem/087C.cpp
+++ atCoder/pastProblem/087C.cpp
@@ -6,44 +6,34 @@ int main() {
   cin >> n;
   vector<int> uup;
   vector<int> ddown;
-  int up_sum = 0;
-  int down_sum = 0;
+  int sum = 0;
 
   for (int k = 0; k < n; k++) {
     int t;
     cin >> t;
-    up_sum += t;
     uup.push_back(t);
   }
-  long long int ans = uup[0];
+  sum = uup[0];
+  int max_sum = sum;
 
   ddown.push_back(0);
   for (int k = 0; k < n; k++) {
     int t;
     cin >> t;
-    down_sum += t;
     ddown.push_back(t);
   }
 
-  int diff;
-  int max_diff = -99;
-  int max_pos = 0;
   for (int k = 1; k < n; k++) {
-    up_sum += uup[k];
-    down_sum += ddown[k];
-    diff = up_sum - down_sum;
-    if (diff > max_diff) {
-      max_diff = diff;
-      max_pos = k;
+    for (int l = 1; l < n; l++) {
+      if (l >= k) sum += ddown[l];
+      else sum += uup[l];
     }
+    if (max_sum < sum) max_sum = sum;
+    sum = uup[0];
   }
+  max_sum += ddown[(ddown.size()-1)];
 
-  for (int k = 1; k < n; k++) {
-    if (max_pos < k) ans += ddown[k];
-    else ans += uup[k];
-  }
-  ans += ddown[(ddown.size()-1)];
-  cout << ans << endl;
+  cout << max_sum << endl;
 
   return 0;
 }
