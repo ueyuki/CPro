972c8b504fd4a2f9719b2f893f6ac20bf1b1ff53
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-05-13 23:36:11 +0900

    AC ABC 097 C

--- atCoder/beginnerContest/097/c.cpp
+++ atCoder/beginnerContest/097/c.cpp
@@ -7,6 +7,14 @@ int main() {
   cin >> s;
   cin >> pos;
 
-  s = pos + '0';
-  cout << s << endl;
+  vector<string> t;
+
+  for (int k = 0; k < (int)s.size(); k++) {
+    for (int l = 0; l < 5; l++) {
+      t.push_back(s.substr(k, l+1));
+    }
+  }
+  sort(t.begin(), t.end());
+  unique(t.begin(), t.end());
+  cout << t[pos-1] << endl;
 }
