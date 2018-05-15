6a0de3dea1a6bbfdc6ab82a25d84b8d077c88bcb
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-05-15 23:48:24 +0900

    Will solve ABC 097 D

--- atCoder/beginnerContest/097/d.cpp
+++ atCoder/beginnerContest/097/d.cpp
@@ -7,15 +7,4 @@ int main() {
   int pair[m][m];
 
   cin >> n >> m;
-  
-  for (int k = 0; k < n; k++) {
-    int t;
-    cin >> t;
-    p.push_back(t);
-  }
-  for (int k = 0; k < m; k++) {
-    for (int l = 0; l < m; l++) {
-      cin >> pair[k][l];
-    }
-  } 
 }
