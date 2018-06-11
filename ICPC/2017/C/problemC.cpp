2b01521e3b7656e21d0e4730ddf3f252f64f7201
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-06-11 23:11:43 +0900

    Start ICPC2017 problem C

---
+++ ICPC/2017/C/problemC.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int d, w;
+  cin >> d >> w;
+  e[d][w];
+
+  for (int k = 0; k < d; k++) {
+    for (int l = 0; l < w; l++) {
+      cin >> e[k][l];
+    }
+  }
+
+  for (int x = 1; x < d-1; x++) {
+    for (int y = 1; y < w-1; y++) {
+      for (int dx = 0; dx < w-x; dx++;) {
+        for (int dy = 0; dy < w-y; dy++;) {
+        }
+      }
+    }
+  }
+}
