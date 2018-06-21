26a60dd27f6d92dac1a7735baca147678747293b
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-06-21 16:56:20 +0900

    Refactor ABC 100 B

--- atCoder/beginnerContest/100/b.cpp
+++ atCoder/beginnerContest/100/b.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int main() {
-  long long int d, n;
+  int d, n;
   cin >> d >> n;
-  long long int handred = 1;
+  int hundred = 1;
   for (int k = 0; k < d; k++) {
-    handred *= 100;
+    hundred *= 100;
   }
-  long long int ans = handred * n;
-  if (n == 100 && d == 0) cout << ans + 1 << endl;
-  else if (n == 100 && d == 1) cout << ans + 100 << endl;
-  else if (n == 100 && d == 2) cout << ans + 10000 << endl;
-  else cout << ans << endl;
+
+  int ans = hundred * n;
+  cout << (n != 100 ? ans : ans + hundred) << endl;
 }
