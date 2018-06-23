8c86d3ffc4c1bfc0fcff8653a066a45ff3b1efd0
ueyuki <s18g459@stu.kagawa-u.ac.jp>
2018-06-24 03:16:53 +0900

    Slove ABC 101 D

--- atCoder/beginnerContest/101/d.cpp
+++ atCoder/beginnerContest/101/d.cpp
@@ -1,41 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  long long int k;
-  cin >> k;
-  long long int s = 0;
-  long long int n = 9;
+long long f(long long n) {
+  long long res = 0;
+  while (n > 0) {
+    res += n % 10;
+    n /= 10;
+  }
+  return res;
+}
 
-  while (k > 0) {
-    vector<long long int> aans;
-    aans.push_back(n);
-    long long int tmp = n;
-    long long int judge = n;
-    while (tmp > 0) {
-      s += (tmp % 10);
-      tmp /= 10;
+double g(long long n) {
+  return (double)(n)/f(n);
+}
+
+int main() {
+  vector<long long> res;
+  long long base = 1;
+
+  // まずは候補を (1〜150)99999 的なやつを全列挙
+  for (int k = 0; k < 15; k++) {
+    for (int l = 1; l < 150; l++) {
+      res.push_back(base * (l + 1) - 1);
     }
-    long long int line = n / s;
-    while (1) {
-      judge -= 1;
-      s -= 1;
-      if (s == 0) break;
-      if ((judge / s) == line) {
-        aans.push_back(judge);
-      }
-      else {
+    base *= 10;
+  }
+  sort(res.begin(), res.end());
+  res.erase(unique(res.begin(), res.end()), res.end());
+
+  // ダメなやつを除く
+  for (long long k = 0; k < res.size(); k++) {
+    for (long long l = k + 1; l < res.size(); l++) {
+      if (g(res[k]) > g(res[l])) {
+        res.erase(res.begin() + k--);
         break;
       }
     }
+  }
 
-    sort(aans.begin(), aans.end());
-    for (long long int l = 0; l < (long long int)aans.size(); l++) {
-      cout << aans[l] << endl;
-      k--;
-      if (k == 0) return 0;
-    }
-    n += 10;
-    s = 0;
+  long long num;
+  cin >> num;
+  for (long long k = 0; k < num; k++) {
+    cout << res[k] << endl;
   }
 }
